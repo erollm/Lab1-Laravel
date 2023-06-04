c675578768a98dbd16a61bb6a84a2d67a47515ef
AlbanSahitii <109045631+AlbanSahitii@users.noreply.github.com>
2023-06-05 01:50:02 +0200

    Fillables added in movies model

--- app/Models/movie.php
+++ app/Models/movie.php
@@ -7,5 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class movie extends Model
 {
+
+
+    // $table->string('title')->unique();
+    // $table->string('backdoor_path');
+    // $table->string('poster_path');
+    // $table->time('length');
+    // $table->date('date');
+    // $table->string('description');
+    // $table->string('trailer');
+    // $table->string('thumbnail');
+    // $table->string('website_name');
+    // $table->string('website_url');
+
+    protected $fillable = ['title', 'backdoor_path', 'poster_path', 'length', 'date', 'description', 'trailer', 'website_name', 'website_url'];
+
     use HasFactory;
 }
