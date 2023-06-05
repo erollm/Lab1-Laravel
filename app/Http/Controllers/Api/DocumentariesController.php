e9f4e5efabdced3bf335f21438660b77ee62281f
AlbanSahitii <109045631+AlbanSahitii@users.noreply.github.com>
2023-06-05 17:51:14 +0200

    Api Controller done.

--- app/Http/Controllers/Api/DocumentariesController.php
+++ app/Http/Controllers/Api/DocumentariesController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\genre;
 use Illuminate\Http\Request;
 
-class DocumentariesController extends Controller
+class ActionController extends Controller
 {
-    //
+    public function __invoke(){
+        $genre = genre::where('genre', 'Documentaries')->first();
+        $movies = $genre->movies;
+
+        if($movies->isEmpty()){
+            return response()->json('Invalid movie');
+        }
+        
+        return response()->json($movies);
+    }
 }
--- app/Http/Controllers/Api/RomanceController.php
+++ app/Http/Controllers/Api/RomanceController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\genre;
 use Illuminate\Http\Request;
 
-class RomanceController extends Controller
+class ActionController extends Controller
 {
-    //
+    public function __invoke(){
+        $genre = genre::where('genre', 'Romance')->first();
+        $movies = $genre->movies;
+
+        if($movies->isEmpty()){
+            return response()->json('Invalid movie');
+        }
+        
+        return response()->json($movies);
+    }
 }
