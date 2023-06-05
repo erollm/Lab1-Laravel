74fc576a1412d1dbb181dafbc2ee04999a74d61b
AlbanSahitii <109045631+AlbanSahitii@users.noreply.github.com>
2023-06-05 17:49:41 +0200

    Api routes intialized

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\ActionController;
+use App\Http\Controllers\Api\ComedyController;
+use App\Http\Controllers\Api\DocumentariesController;
+use App\Http\Controllers\Api\HorrorController;
 use App\Http\Controllers\Api\MovieController;
+use App\Http\Controllers\Api\RomanceController;
 use App\Http\Controllers\Api\TrendingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,3 +18,7 @@ Route::group(['prefix' => 'v1'], function(){
 
 Route::get('/movies/trending', TrendingController::class);
 Route::get('/movies/Action', ActionController::class);
+Route::get('/movies/Comedy', ComedyController::class);
+Route::get('/movies/Horror', HorrorController::class);
+Route::get('/movies/Romance', RomanceController::class);
+Route::get('/movies/Documentaries', DocumentariesController::class);
